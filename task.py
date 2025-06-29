9a0b4e409777d879c57005541a4460711cc26a2b
cloner1998 <sajjadyami77@gmail.com>
2025-06-29 23:33:00 +0330

    implement task class

---
+++ task.py
@@ -0,0 +1,25 @@
+from enums.priority import Priority
+
+class Task:
+    def __init__(self, name, description, priority):
+        self.name = name
+        self.description = description
+        if not isinstance(priority, Priority):
+            raise TypeError("Priority must be of type Priority")
+        self.priority = priority
+
+
+    def update_priority(self, priority):
+        self.priority = priority
+        return self
+
+    def update_name(self, name):
+        self.name = name
+        return self
+
+    def update_description(self, description):
+        self.description = description
+        return self
+
+    def __str__(self):
+        return f"Task {self.name}, priority: {self.priority}, description: {self.description}"
